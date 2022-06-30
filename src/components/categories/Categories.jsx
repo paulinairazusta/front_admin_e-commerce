0795c159574cc1c2ff19e7c370f40cd4437bea76
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-30 16:59:22 -0300

    create category

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
+import "./categories.css";
 
 function Categories() {
   const [categories, setCategories] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
 
   useEffect(() => {
     async function getCategories() {
-      const response = await Axios.get("http://localhost:3001/api/categories");
+      const response = await Axios.get(`http://localhost:3001/api/categories`);
       setCategories(response.data);
     }
     getCategories();
   }, []);
 
+  async function handleCreate(event) {
+    // event.preventDefault(); Se lo comenté porque creo que en este caso sí conviene que recargue la página así cuando se recarga ya aparece la categoría nueva. Si no, no aparece y hay que recargar a mano.
+    await Axios.post("http://localhost:3001/create/category", {
+      name: categoryName,
+    });
+  }
+
   return (
     <>
       <button className="btn btn-light">New category</button>
-      {categories.map((categorie) => {
+      <form action="" onSubmit={handleCreate}>
+        <input
+          type="text"
+          value={categoryName}
+          onChange={(event) => setCategoryName(event.target.value)}
+        />
+        <button type="submit">Create category</button>
+      </form>
+
+      {categories.map((category) => {
         return (
-          <ul className="list-unstyled">
-            <li>{categorie.name}</li>
+          <ul className="list-unstyled" key={category._id}>
+            <li>{category.name}</li>
           </ul>
         );
       })}
