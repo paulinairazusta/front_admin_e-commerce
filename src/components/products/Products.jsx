ee6418cf2e9b1688385279def7cd7433e18d9503
flofal <florencia.falco93@gmail.com>
2022-07-01 12:22:38 -0300

    product list

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
+import "./products.css";
+import { Table } from "react-bootstrap";
+import { BiEdit } from "react-icons/bi";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function Products() {
   const [products, setProducts] = useState([]);
@@ -13,16 +17,51 @@ function Products() {
   }, []);
 
   return (
-    <>
-      <button className="btn btn-light">New product</button>
-      {products.map((product) => {
-        return (
-          <ul className="list-unstyled">
-            <li>{product.name}</li>
-          </ul>
-        );
-      })}
-    </>
+    <div className="content">
+      <div className="products-card">
+        <div className="header-container ">
+          <strong>Products</strong>
+          <button className="btn-new-product">New product</button>
+        </div>
+        <div className="table-products-container">
+          <Table hover>
+            <thead>
+              <tr>
+                <th></th>
+                <th>TITLE</th>
+                <th>PRICE</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => {
+                console.log(product.image);
+                return (
+                  <>
+                    <tr key="id" className="align-middle">
+                      <td>
+                        <img
+                          className="product-img"
+                          src={`http://localhost:3001/images/${product.image}`}
+                        />
+                      </td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>
+                        <div className="edit-delete-icons">
+                          <BiEdit />
+                          <RiDeleteBinLine />
+                        </div>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
   );
 }
 
