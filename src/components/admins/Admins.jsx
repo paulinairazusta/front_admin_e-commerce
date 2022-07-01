53748a1a8f421fef8c0c4d0e291a7ddfbdc68e38
flofal <florencia.falco93@gmail.com>
2022-07-01 12:51:57 -0300

    Admin/Client table

--- src/components/admins/Admins.jsx
+++ src/components/admins/Admins.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
+import "../products/products.css";
+
+import { Table } from "react-bootstrap";
+import { BiEdit } from "react-icons/bi";
+import { RiDeleteBinLine } from "react-icons/ri";
+
 function Admins() {
   const [admins, setAdmins] = useState([]);
 
@@ -16,7 +22,51 @@ function Admins() {
 
   return (
     <>
-      {admins.map((admin) => {
+      <div className="content">
+        <div className="products-card">
+          <div className="header-container ">
+            <strong>Admins</strong>
+            <button className="btn-new-product">New admin</button>
+          </div>
+          <div className="table-products-container">
+            <Table hover>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>FIRST NAME</th>
+                  <th>LAST NAME</th>
+                  <th>ACTIONS</th>
+                </tr>
+              </thead>
+              <tbody>
+                {admins.map((admin) => {
+                  return (
+                    <>
+                      <tr key={admin._id} className="align-middle">
+                        <td>
+                          <img
+                            className="product-img"
+                            src={`http://localhost:3001/images/${admin.image}`}
+                          />
+                        </td>
+                        <td>{admin.firstname}</td>
+                        <td>{admin.lastname}</td>
+                        <td>
+                          <div className="edit-delete-icons">
+                            <BiEdit />
+                            <RiDeleteBinLine />
+                          </div>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+      {/* {admins.map((admin) => {
         return (
           <ul key={admin._id}>
             <li>
@@ -24,7 +74,7 @@ function Admins() {
             </li>
           </ul>
         );
-      })}
+      })} */}
     </>
   );
 }
--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
+import "../products/products.css";
+
+import { Table } from "react-bootstrap";
+import { BiEdit } from "react-icons/bi";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function Clients() {
   const [clients, setClients] = useState([]);
@@ -14,7 +19,52 @@ function Clients() {
 
   return (
     <>
-      {clients.map((client) => {
+      <div className="content">
+        <div className="products-card">
+          <div className="header-container ">
+            <strong>Clients</strong>
+            {/* <button className="btn-new-product">New product</button> */}
+          </div>
+          <div className="table-products-container">
+            <Table hover>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>FIRST NAME</th>
+                  <th>LAST NAME</th>
+                  <th>ACTIONS</th>
+                </tr>
+              </thead>
+              <tbody>
+                {clients.map((client) => {
+                  return (
+                    <>
+                      <tr key={client._id} className="align-middle">
+                        <td>
+                          <img
+                            className="product-img"
+                            src={`http://localhost:3001/images/${client.image}`}
+                          />
+                        </td>
+                        <td>{client.firstname}</td>
+                        <td>{client.lastname}</td>
+                        <td>
+                          <div className="edit-delete-icons">
+                            <BiEdit />
+                            <RiDeleteBinLine />
+                          </div>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+
+      {/* {clients.map((client) => {
         return (
           <ul className="list-unstyled" key={client._id}>
             <li>
@@ -22,7 +72,7 @@ function Clients() {
             </li>
           </ul>
         );
-      })}
+      })} */}
     </>
   );
 }
