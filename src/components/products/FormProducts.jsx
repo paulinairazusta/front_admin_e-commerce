e49f8075da98ed1e8e79d84f448629b38f6c216b
flofal <florencia.falco93@gmail.com>
2022-07-01 14:57:00 -0300

    Update FormProducts.jsx

--- src/components/products/FormProducts.jsx
+++ src/components/products/FormProducts.jsx
@@ -1,60 +1,79 @@
 import axios from "axios";
 import { useState } from "react";
+import "../register/register.css";
 
 function FormProducts() {
-	const [name, setName] = useState("");
-	const [description, setDescription] = useState("");
-	const [price, setPrice] = useState("");
-	const [image, setImage] = useState("");
-	const [category, setCategory] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [category, setCategory] = useState("");
 
-	const createProductHandler = async () => {
-		const response = await axios.post("http://localhost:3001/api/product", {
-			name,
-			description,
-			price,
-			image,
-		});
-		console.log(response.data);
-	};
-	return (
-		<div>
-			<form
-				onSubmit={(event) => {
-					event.preventDefault();
-					createProductHandler();
-				}}>
-				<label htmlFor=''>Product Name</label>
-				<input
-					value={name}
-					onChange={(event) => setName(event.target.value)}
-					type='text'
-					id=''
-				/>
-				<label htmlFor=''>Description</label>
-				<textarea
-					value={description}
-					onChange={(event) => setDescription(event.target.value)}
-					id=''
-					cols='30'
-					rows='10'></textarea>
-				<label htmlFor=''>Price</label>
-				<input
-					value={price}
-					onChange={(event) => setPrice(event.target.value)}
-					type='tel'
-				/>
-				<label htmlFor=''>Product Image</label>
-				<input
-					onChange={(event) => setImage(event.target.files[0].name)}
-					type='file'
-					id=''
-				/>
+  const createProductHandler = async () => {
+    const response = await axios.post("http://localhost:3001/api/product", {
+      name,
+      description,
+      price,
+      image,
+    });
+    console.log(response.data);
+  };
+  return (
+    <div className="register-container">
+      <div>
+        <h2>New product</h2>
+        <form
+          className="register-card"
+          onSubmit={(event) => {
+            event.preventDefault();
+            createProductHandler();
+          }}
+        >
+          <label className="label" htmlFor="">
+            Product Name
+          </label>
+          <input
+            className="input"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            type="text"
+            id=""
+          />
+          <label className="label" htmlFor="">
+            Description
+          </label>
+          <textarea
+            className="input"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <label className="label" htmlFor="">
+            Price
+          </label>
+          <input
+            className="input"
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+            type="tel"
+          />
+          <label className="label" htmlFor="">
+            Product Image
+          </label>
+          <input
+            className="input"
+            onChange={(event) => setImage(event.target.files[0].name)}
+            type="file"
+            id=""
+          />
 
-				<button>Send</button>
-			</form>
-		</div>
-	);
+          <button className="button-submit">Add a product</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default FormProducts;
