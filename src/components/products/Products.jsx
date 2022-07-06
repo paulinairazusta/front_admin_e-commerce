b0bcd6ca6d23e167d3f749497d4508ca0a21258a
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-07-06 15:00:03 -0300

    update products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import OffCanvas from "../sidebar/OffCanvas";
 import { useDispatch } from "react-redux";
 
@@ -10,106 +11,108 @@ import { BiEdit } from "react-icons/bi";
 import { storeProduct } from "../../redux/productSlice";
 
 function Products() {
-	const [products, setProducts] = useState([]);
-	const [checked, setChecked] = useState(true);
-	const [showMenu, setShowMenu] = useState(true);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(true);
+  const [showMenu, setShowMenu] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const toggleProductHandler = async (productId, isAvailable) => {
-		const response = await Axios.delete(
-			`${process.env.REACT_APP_API_URL}/product`,
-			{ data: { productId, isAvailable: checked } }
-		);
-	};
+  const admin = useSelector((state) => state.admin);
 
-	const config = {
-		headers: {
-			Authorization: "Bearer " + process.env.REACT_APP_ADMIN_TOKEN,
-		},
-	};
+  const toggleProductHandler = async (productId, isAvailable) => {
+    const response = await Axios.delete(
+      `${process.env.REACT_APP_API_URL}/product`,
+      { data: { productId, isAvailable: checked } }
+    );
+  };
 
-	useEffect(() => {
-		async function getProducts() {
-			const response = await Axios.get(
-				`${process.env.REACT_APP_API_URL}/api/products`,
-				config
-			);
-			setProducts(response.data);
-			dispatch(storeProduct({ products: response.data }));
-		}
+  const config = {
+    headers: {
+      Authorization: "Bearer " + admin.token,
+    },
+  };
 
-		getProducts();
-	}, []);
+  useEffect(() => {
+    async function getProducts() {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/products`,
+        config
+      );
+      setProducts(response.data);
+      dispatch(storeProduct({ products: response.data }));
+    }
 
-	return (
-		<>
-			<div className={`content${showMenu ? " with-margin" : ""}`}>
-				<OffCanvas show={showMenu} setShow={setShowMenu} />
-				<div className='products-card'>
-					<div className='header-container '>
-						<strong>Products</strong>
-						<Link to='/newproduct'>
-							<button className='btn-new-product'>New product</button>
-						</Link>
-					</div>
-					<div className='table-products-container'>
-						<Table hover>
-							<thead>
-								<tr>
-									<th></th>
-									<th>TITLE</th>
-									<th>PRICE</th>
-									<th>STOCK</th>
-									<th>ACTIONS</th>
-									<th>AVAILABLE</th>
-								</tr>
-							</thead>
-							<tbody>
-								{products.map((product) => {
-									return (
-										<tr key={product._id} className='align-middle'>
-											<td>
-												<img
-													className='product-img'
-													src={`https://tkyarzymrutnhhccfvhu.supabase.co/storage/v1/object/public/images/${product.image}`}
-													alt='product'
-												/>
-											</td>
-											<td>{product.name}</td>
-											<td>{product.price}</td>
-											<td>{product.stock}</td>
-											<td>
-												<div className='edit-delete-icons'>
-													<BiEdit
-														onClick={() => {
-															navigate(`/edit/${product._id}`);
-														}}
-													/>
-												</div>
-											</td>
-											<td>
-												<div>
-													<input
-														defaultChecked={checked}
-														type='checkbox'
-														onChange={() => {
-															toggleProductHandler(product._id);
-															setChecked(!checked);
-														}}
-													/>
-												</div>
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</Table>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <div className={`content${showMenu ? " with-margin" : ""}`}>
+        <OffCanvas show={showMenu} setShow={setShowMenu} />
+        <div className="products-card">
+          <div className="header-container ">
+            <strong>Products</strong>
+            <Link to="/newproduct">
+              <button className="btn-new-product">New product</button>
+            </Link>
+          </div>
+          <div className="table-products-container">
+            <Table hover>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>TITLE</th>
+                  <th>PRICE</th>
+                  <th>STOCK</th>
+                  <th>ACTIONS</th>
+                  <th>AVAILABLE</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product) => {
+                  return (
+                    <tr key={product._id} className="align-middle">
+                      <td>
+                        <img
+                          className="product-img"
+                          src={`https://tkyarzymrutnhhccfvhu.supabase.co/storage/v1/object/public/images/${product.image}`}
+                          alt="product"
+                        />
+                      </td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.stock}</td>
+                      <td>
+                        <div className="edit-delete-icons">
+                          <BiEdit
+                            onClick={() => {
+                              navigate(`/edit/${product._id}`);
+                            }}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div>
+                          <input
+                            defaultChecked={checked}
+                            type="checkbox"
+                            onChange={() => {
+                              toggleProductHandler(product._id);
+                              setChecked(!checked);
+                            }}
+                          />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Products;
